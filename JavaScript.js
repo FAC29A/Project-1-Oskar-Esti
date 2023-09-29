7d91cd1f07ee9656ef8cfb3a37d17f54b2f6bb9f
Esti <stsh7573@gmail.com>
2023-09-29 15:51:08 +0100

    adding

---
+++ JavaScript.js
@@ -0,0 +1,15 @@
+
+
+// ---------------Scroll reveal----------------------
+
+
+ScrollReveal({
+    reset: true,
+    distance: "80px",
+    duration: 2000,
+    delay: 200,
+  });
+  
+  ScrollReveal().reveal(".h2, .card-container, .left", {
+    origin: "top",
+  });
