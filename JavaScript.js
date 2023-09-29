881d703df280148d24d69f23a8f6c3754891b302
Esti <stsh7573@gmail.com>
2023-09-29 16:05:51 +0100

    adding Javascript

--- JavaScript.js
+++ JavaScript.js
@@ -10,6 +10,16 @@ ScrollReveal({
     delay: 200,
   });
   
-  ScrollReveal().reveal(".h2, .card-container, .left", {
+  ScrollReveal().reveal(".h2, .card-container, heading-container h2 span", {
     origin: "top",
+  });
+
+  ScrollReveal().reveal("",
+    { origin: "bottom" }
+  );
+  ScrollReveal().reveal(" .btn, .card-container, .left", {
+    origin: "left",
+  });
+  ScrollReveal().reveal(".right, .introduction-container", {
+    origin: "right",
   });
